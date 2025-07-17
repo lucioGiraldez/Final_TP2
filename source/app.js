95273ddfa104567a6a5e1f2815e686247d81c0db
Lucio Giraldez <luciogiraldez@gmail.com>
2025-07-16 21:24:10 -0300

    Segundo commit, codigo emprolijado

--- source/app.js
+++ source/app.js
@@ -1,15 +1,13 @@
 import express from "express";
 import { config } from "./config/config.js";
 import { productRouter } from "./routes/product.router.js";
-import { statusRouter } from "./routes/status.router.js";
 
 const app = express();
 app.use(express.json());
 
-app.use("/api", statusRouter);
 app.use("/api", productRouter);
 
 app.listen(config.PORT, () => {
-	const message = `👏🚀SERVER FUNCIONANDO CORRECTAMENTE EN http://${config.HOST}:${config.PORT}`;
+	const message = `🎉🎉SERVER FUNCIONANDO CORRECTAMENTE EN http://${config.HOST}:${config.PORT}`;
 	console.log(message);
 });
